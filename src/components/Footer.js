4ae0da774f3319d7a0c3094b32ac26f47d992749
anuD <adilbagi22@gmail.com>
2023-09-17 16:52:13 -0700

    Added 2 more images for Portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,8 @@ export default function Footer() {
       <nav>
         <ul className="nav d-flex justify-content-center">
           <li className="nav-item">
-            <a
-              href="https://github.com/anud22"
-              className="nav-link text-white"
-            >
-              GitHub
+            <a href="https://github.com/anud22" className="nav-link text-white">
+            <img src="github.png" alt="GitHub" width="45" height="45" />
             </a>
           </li>
           <li className="nav-item">
@@ -18,7 +15,7 @@ export default function Footer() {
               href="https://www.linkedin.com/in/anuradha-dilbagi-5a67a3125/"
               className="nav-link text-white"
             >
-              LinkedIn
+              <img src="li.png" alt="LinkedIn" width="45" height="45" />
             </a>
           </li>
         </ul>
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -7,46 +7,102 @@ export default function Project() {
         <p className="display-4 mb-4 text-center">Portfolio</p>
       </div>
       <div className="d-flex flex-row justify-content-around mb-4 mx-4">
-        <div className="col-md-4 d-flex align-items-center justify-content-end">
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
           <div style={{ height: "500px", width: "500px" }}>
-            <img
-              src="fundraiser.png"
-              alt="Fundraiser Project"
-              className="img-fluid"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
+            <a
+              href="https://mvc-project-2-9329043fdd43.herokuapp.com/"
+              data-toggle="tooltip"
+              data-placement="top"
+              title="Fundraiser Project- MVC, Javascript, Bootstrap, Express, SQLServer, Sequilize"
+            >
+              <img
+                src="fundraiser.png"
+                alt="Fundraiser Project"
+                className="img-fluid"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </a>
           </div>
         </div>
-        <div className="col-md-4 d-flex align-items-center justify-content-start">
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
           <div style={{ height: "500px", width: "500px" }}>
-            <img
-              src="TrendWatch.png"
-              alt="Media Project"
-              className="img-fluid"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
+            <a
+              href="https://anud22.github.io/TrendWatch/"
+              data-placement="top"
+              title="Trendwatch Project- HTML, CSS, JQuery, Javascript"
+            >
+              <img
+                src="TrendWatch.png"
+                alt="Media Project"
+                className="img-fluid"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
+          <div style={{ height: "500px", width: "500px" }}>
+            <a
+              href="https://anud22.github.io/WeatherForecast/"
+              data-toggle="tooltip"
+              data-placement="top"
+              title="Weather Forecast Project-Javascript, API, Bootstrap, Express, Local Storage"
+            >
+              <img
+                src="weather.png"
+                alt="Weather Forecast Project"
+                className="img-fluid"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </a>
           </div>
         </div>
       </div>
       <div className="d-flex flex-row justify-content-around mb-4 mx-4">
-        <div className="col-md-4 d-flex align-items-center justify-content-end">
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
           <div style={{ height: "500px", width: "500px" }}>
-            <img
-              src="iot.jpg"
-              alt="IOT"
-              className="img-fluid"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
+            <a
+              href="https://anud22.github.io/module1Challenge/"
+              data-placement="top"
+              title="Mern Project- Javascript, React, Mongo, GraphQL, Express"
+            >
+              <img
+                src="iot.jpg"
+                alt="IOT"
+                className="img-fluid"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </a>
           </div>
         </div>
-        <div className="col-md-4 d-flex align-items-center justify-content-start">
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
           <div style={{ height: "500px", width: "500px" }}>
-            <img
-              src="todo.jpg"
-              alt="To Do List"
-              className="img-fluid"
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
+            <a href="https://mighty-stream-60543-b28aad666d1f.herokuapp.com/"    data-placement="top"
+              title="Javascript, LocalServer, Bootstrap, JQuery">
+              <img
+                src="todo.jpg"
+                alt="To Do List"
+                className="img-fluid"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </a>
+          </div>
+        </div>
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
+          <div style={{ height: "500px", width: "500px" }}>
+            <a
+              href="https://mvc-project-2-9329043fdd43.herokuapp.com/"
+              data-toggle="tooltip"
+              data-placement="top"
+              title="Workday Scheduler Project-  Javascript, Bootstrap, Local storage"
+            >
+              <img
+                src="scheduler.png"
+                alt="WorkdayScheduler Project"
+                className="img-fluid"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </a>
           </div>
         </div>
       </div>
